70bcf78b1bf6d46d58594a37543528354d0e0c91
dedeye <dedeye-wrk@yandex.ru>
2026-08-14 18:45:58 +0600

    feat: sync migrations

--- scripts/migrate-down.ts
+++ scripts/migrate-down.ts
@@ -3,5 +3,9 @@ import Database from 'better-sqlite3'
 const db = new Database('news.db')
 
 db.exec(`
+  DROP TABLE IF EXISTS news_content
+`)
+
+db.exec(`
   DROP TABLE IF EXISTS news
 `)
--- scripts/migrate-up.ts
+++ scripts/migrate-up.ts
@@ -12,3 +12,18 @@ db.exec(`
     pubDate TEXT NOT NULL
   )
 `)
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS news_content (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    news_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    mood TEXT NOT NULL,
+    FOREIGN KEY (news_id) REFERENCES news (id)
+  )
+`)
+
+db.exec(`
+  CREATE UNIQUE INDEX IF NOT EXISTS idx_news_content_unique
+  ON news_content (news_id, content);
+`)
