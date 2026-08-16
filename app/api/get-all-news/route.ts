44eb31ce14098714c9b78c9693dccfc7e0788256
dedeye <dedeye-wrk@yandex.ru>
2026-08-16 15:06:59 +0600

    feat: add check of news list

--- app/api/get-all-news/route.ts
+++ app/api/get-all-news/route.ts
@@ -4,5 +4,9 @@ import { NextResponse } from "next/server"
 export function GET() {
   const newsList = getAllNewsDb()
 
+  if (!newsList || newsList.length === 0) {
+    return NextResponse.json({ error: 'NOT_FOUND', message: 'There is no news list' }, { status: 404 })
+  }
+
   return NextResponse.json(newsList, { status: 200 })
 }
